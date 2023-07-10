dc7def94fb9fb065e4315adc4d61d8ca987227fd
Michael Golden <Michael.A.Golden@gmail.com>
2023-07-10 16:50:06 -0500

    Moved functions out of transaction class, class reserved for future transaction data type

--- trade.py
+++ trade.py
@@ -2,48 +2,51 @@ from __future__ import annotations
 import validators as v
 
 
+def set_inventory(seller, buyer, item_name, item_qty):
+    # handles inventory transactio between buyer and seller, inventory updates in same function
+    prior_seller_item_qty = getattr(seller, item_name)
+    updated_seller_item_qty = prior_seller_item_qty - item_qty
+    setattr(seller, item_name, updated_seller_item_qty)
+
+    prior_buyer_item_qty = getattr(buyer, item_name)
+    updated_buyer_item_qty = prior_buyer_item_qty + item_qty
+    setattr(buyer, item_name, updated_buyer_item_qty)
+
+
+def set_gold(seller, buyer, total_trade):
+    # sets buyer and seller new gold values in same function to ensure completion
+    prior_seller_gold = getattr(seller, 'gold')
+    updated_seller_gold = prior_seller_gold + total_trade
+    setattr(seller, 'gold', updated_seller_gold)
+
+    prior_buyer_gold = getattr(buyer, 'gold')
+    updated_buyer_gold = prior_buyer_gold - total_trade
+    setattr(buyer, 'gold', updated_buyer_gold)
+
+
+def log_transaction(player, market, item_name, item_price, item_qty):
+    pass
+
+
+def trade(player, market, trade_type, item_name, item_price, item_qty):
+    total_trade = item_price * item_qty
+    if trade_type == 'buy':
+        v.Validators.affordability_check(total_trade, player, trade)
+        v.Validators.check_inventory_capacity(item_qty, player, trade)
+        set_gold(player, market, total_trade)
+        set_inventory(player, market, item_name, item_qty)
+        log_transaction(player, market, item_name,
+                        item_qty, item_price, total_trade)
+    elif trade_type == 'sell':
+        set_gold(market, player)
+        set_inventory(market, player)
+        log_transaction(market, player, item_name,
+                        item_qty, item_price)
+
+
 class Transaction:
-
-    def __init__(self):
-        pass
-
-    def __repr__(self):
-        pass
-
-    def set_inventory(self, seller, buyer, item_name, item_qty):
-        # handles inventory transactio between buyer and seller, inventory updates in same function
-        prior_seller_item_qty = getattr(seller, item_name)
-        updated_seller_item_qty = prior_seller_item_qty - item_qty
-        setattr(seller, item_name, updated_seller_item_qty)
-
-        prior_buyer_item_qty = getattr(buyer, item_name)
-        updated_buyer_item_qty = prior_buyer_item_qty + item_qty
-        setattr(buyer, item_name, updated_buyer_item_qty)
-
-    def set_gold(self, seller, buyer, total_trade):
-        # sets buyer and seller new gold values in same function to ensure completion
-        prior_seller_gold = getattr(seller, 'gold')
-        updated_seller_gold = prior_seller_gold + total_trade
-        setattr(seller, 'gold', updated_seller_gold)
-
-        prior_buyer_gold = getattr(buyer, 'gold')
-        updated_buyer_gold = prior_buyer_gold - total_trade
-        setattr(buyer, 'gold', updated_buyer_gold)
-
-    def log_transaction(self, player, market, item_name, item_price, item_qty):
-        pass
-
-    def trade(self, player, market, trade_type, item_name, item_price, item_qty):
-        total_trade = item_price * item_qty
-        if trade_type == 'buy':
-            v.Validators.affordability_check(total_trade, player, self.trade)
-            v.Validators.check_inventory_capacity(item_qty, player, self.trade)
-            self.set_gold(player, market, total_trade)
-            self.set_inventory(player, market, item_name, item_qty)
-            self.log_transaction(player, market, item_name,
-                                 item_qty, item_price, total_trade)
-        elif trade_type == 'sell':
-            self.set_gold(market, player)
-            self.set_inventory(market, player)
-            self.log_transaction(market, player, item_name,
-                                 item_qty, item_price)
+    """
+     _summary_
+     Transaction class provides class method provides a way to dynamically store transactions between player and markets as its own datatype, encapsulating all information necessary to understand the transaction and is the base input for all logging
+    """
+    pass
